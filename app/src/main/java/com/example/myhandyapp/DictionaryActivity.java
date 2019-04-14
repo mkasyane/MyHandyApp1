9872c1f27584ef7b83e3cf15add7172a83dee592
Max <max@max.com>
2019-04-14 03:58:08 -0400

    added dictionary class

--- app/src/main/java/com/example/myhandyapp/DictionaryActivity.java
+++ app/src/main/java/com/example/myhandyapp/DictionaryActivity.java
@@ -1,8 +1,16 @@
 package com.example.myhandyapp;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.example.myhandyapp.listitems.Flight;
+
+import java.util.List;
 
 public class DictionaryActivity extends CommonActivity {
+
+    ProgressBar progressBar;
+    private List<Flight> flightList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
